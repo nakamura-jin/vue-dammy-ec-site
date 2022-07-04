7b7cc57e419cc1d98cf5df33c21fdd0d8d6db9ec
中村　迅 <shu.kyu0224@icloud.com>
2022-07-04 17:15:29 +0900

    トップ画面作成

--- src/main.js
+++ src/main.js
@@ -3,6 +3,15 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// font awesome
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faCartShopping);
+
+Vue.component('font-awesome-icon', FontAwesomeIcon);
+
 Vue.config.productionTip = false
 
 new Vue({
---
+++ src/views/Root.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="root">
+    <the-header />
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import TheHeader from '@/components/TheHeader.vue'
+export default {
+  components: {
+    TheHeader
+  }
+}
+</script>
