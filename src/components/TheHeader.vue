1629e596b08b9277978b580a81702accf2de763e
中村　迅 <shu.kyu0224@icloud.com>
2022-07-04 14:41:31 +0900

    タイトルにリンク追加

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -4,7 +4,7 @@
 <template>
   <div class="header">
     <div class="header__container">
-      <h2 class="header__title">Dammy Take Out</h2>
+      <router-link to="/"><h2 class="header__title">Dammy Take Out</h2></router-link>
       <div class="header__menu" @click="menu = !menu" :class="{ 'open' : menu }">
         <div class="top"></div>
         <div class="middle"></div>
