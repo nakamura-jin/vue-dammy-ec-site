730f7b2d7c54fdb0d278fc9bfb48adc9e8d0cd6b
中村　迅 <shu.kyu0224@icloud.com>
2022-07-04 14:32:32 +0900

    ヘッダー作成

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,25 @@
+<style lang="scss" scoped>
+  @use '@/styles/header';
+</style>
+<template>
+  <div class="header">
+    <div class="header__container">
+      <h2 class="header__title">Dammy Take Out</h2>
+      <div class="header__menu" @click="menu = !menu" :class="{ 'open' : menu }">
+        <div class="top"></div>
+        <div class="middle"></div>
+        <div class="bottom"></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      menu: false
+    }
+  }
+}
+</script>
